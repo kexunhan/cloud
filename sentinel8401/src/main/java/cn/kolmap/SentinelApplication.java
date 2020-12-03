eb5750e0dc6c76401e5d5b092d36624fe7726145
韩可迅 <2215396405@qq.com>
2020-12-03 17:11:29 +0800

    sentinel

---
+++ sentinel8401/src/main/java/cn/kolmap/SentinelApplication.java
@@ -0,0 +1,15 @@
+package cn.kolmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kxhan
+ * @createTime 2020/12/03
+ */
+@SpringBootApplication
+public class SentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+}
---
+++ sentinel8401/src/main/java/cn/kolmap/controller/RateLimitController.java
@@ -0,0 +1,27 @@
+package cn.kolmap.controller;
+
+import cn.kolmap.ExceptionHandler;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RateLimitController {
+    @GetMapping("/A")
+    @SentinelResource(value = "A", blockHandler = "exceptionA")
+    public String A() {
+        return "a";
+    }
+
+    public String exceptionA(BlockException e) {
+        return "exceptionA";
+    }
+
+    @GetMapping("/B")
+    @SentinelResource(value = "B", blockHandlerClass = ExceptionHandler.class, blockHandler = "handler1")
+    public String B() {
+        return "B";
+    }
+}
